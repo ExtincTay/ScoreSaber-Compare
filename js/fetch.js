8230e4e9bbeb53650cd5509068ee653717e2c31c
ExtincTay <52536506+ExtincTay@users.noreply.github.com>
2022-10-29 02:29:13 +0100

    added stuff just need to change css i think

---
+++ js/fetch.js
@@ -0,0 +1,6 @@
+async function get1() {
+  document.getElementById("p1frame").src = "https://scoresaber.com/u/" + txtInp1.value;
+}
+async function get2() {
+  document.getElementById("p2frame").src = "https://scoresaber.com/u/" + txtInp2.value;
+}
