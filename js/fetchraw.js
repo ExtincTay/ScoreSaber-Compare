7b1f878dab1a8b4efcca12c498712f99d69f8118
ExtincTay <52536506+ExtincTay@users.noreply.github.com>
2022-11-10 06:56:08 +0000

    a

--- js/fetchraw.js
+++ js/fetchraw.js
@@ -5,9 +5,9 @@ async function get1() {
     const playerNameNoQuotes1 = playerName1.replace(/"/g, "");
     let textaccavg1 = json1.scoreStats.averageRankedAccuracy;
     let resultaccavg1 = textaccavg1.toString().slice(0, 5);
-    document.getElementById("rank1").innerHTML += `<pre>${JSON.stringify(json1.playerInfo.rank, null, 2)}</pre>`;
-    document.getElementById("name1").innerHTML += playerNameNoQuotes1;
-    document.getElementById("Acc1").innerHTML += resultaccavg1 + "%";
+    document.getElementById("rank1").innerHTML = `<pre>${JSON.stringify(json1.playerInfo.rank, null, 2)}</pre>`;
+    document.getElementById("name1").innerHTML = playerNameNoQuotes1;
+    document.getElementById("Acc1").innerHTML = resultaccavg1 + "%";
 
     const resp2 = await fetch('https://new.scoresaber.com/api/player/' + txtInp2.value + '/full');
     const json2 = await resp2.json();
@@ -15,12 +15,12 @@ async function get1() {
     const playerNameNoQuotes2 = playerName2.replace(/"/g, "");
     let textaccavg2 = json2.scoreStats.averageRankedAccuracy;
     let resultaccavg2 = textaccavg2.toString().slice(0, 5);
-    document.getElementById("rank2").innerHTML += `<pre>${JSON.stringify(json2.playerInfo.rank, null, 2)}</pre>`;
-    document.getElementById("name2").innerHTML += playerNameNoQuotes2;
-    document.getElementById("Acc2").innerHTML += resultaccavg2 + "%";
+    document.getElementById("rank2").innerHTML = `<pre>${JSON.stringify(json2.playerInfo.rank, null, 2)}</pre>`;
+    document.getElementById("name2").innerHTML = playerNameNoQuotes2;
+    document.getElementById("Acc2").innerHTML = resultaccavg2 + "%";
 
     rank1 = JSON.stringify(json1.playerInfo.rank, null, 2);
     rank2 = JSON.stringify(json2.playerInfo.rank, null, 2);
     rankdiff = Number(rank1) - Number(rank2);
-    document.getElementById("rankdiff").innerHTML += rankdiff;
+    document.getElementById("rankdiff").innerHTML = rankdiff;
 }
