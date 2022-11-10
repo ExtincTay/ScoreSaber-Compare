8353ac185b020da749d512e7b7241eb6d4fa63b3
ExtincTay <52536506+ExtincTay@users.noreply.github.com>
2022-11-10 06:39:20 +0000

    aa

--- js/fetchraw.js
+++ js/fetchraw.js
@@ -1,28 +1,26 @@
 async function get1() {
-    const resp = await fetch('https://new.scoresaber.com/api/player/' + txtInp1.value + '/full');
-    const json = await resp.json();
-    const playerName = json.playerInfo.playerName
-    const playerNameNoQuotes = playerName.replace(/"/g, "");
-    const flagcountry = json.playerInfo.country
-    const capscountry = flagcountry.replace(/"/g, "");
-    const country = capscountry.toLowerCase();
-    let textaccavg = json.scoreStats.averageRankedAccuracy;
-    let resultaccavg = textaccavg.toString().slice(0, 5);
-    document.getElementById("rank1").innerHTML += `<pre>${JSON.stringify(json.playerInfo.rank, null, 2)}</pre>`;
+    const resp1 = await fetch('https://new.scoresaber.com/api/player/' + txtInp1.value + '/full');
+    const json1 = await resp1.json();
+    const playerName1 = json1.playerInfo.playerName
+    const playerNameNoQuotes1 = playerName1.replace(/"/g, "");
+    let textaccavg1 = json1.scoreStats.averageRankedAccuracy;
+    let resultaccavg1 = textaccavg1.toString().slice(0, 5);
+    document.getElementById("rank1").innerHTML += `<pre>${JSON.stringify(json1.playerInfo.rank, null, 2)}</pre>`;
     document.getElementById("name1").innerHTML += playerNameNoQuotes;
     document.getElementById("Acc1").innerHTML += resultaccavg + "%";
-}
-async function get2() {
-    const resp = await fetch('https://new.scoresaber.com/api/player/' + txtInp2.value + '/full');
-    const json = await resp.json();
-    const playerName = json.playerInfo.playerName
-    const playerNameNoQuotes = playerName.replace(/"/g, "");
-    const flagcountry = json.playerInfo.country
-    const capscountry = flagcountry.replace(/"/g, "");
-    const country = capscountry.toLowerCase();
-    let textaccavg = json.scoreStats.averageRankedAccuracy;
-    let resultaccavg = textaccavg.toString().slice(0, 5);
-    document.getElementById("rank2").innerHTML += `<pre>${JSON.stringify(json.playerInfo.rank, null, 2)}</pre>`;
+
+    const resp2 = await fetch('https://new.scoresaber.com/api/player/' + txtInp2.value + '/full');
+    const json2 = await resp2.json();
+    const playerName2 = json2.playerInfo.playerName
+    const playerNameNoQuotes2 = playerName2.replace(/"/g, "");
+    let textaccavg2 = json2.scoreStats.averageRankedAccuracy;
+    let resultaccavg2 = textaccavg2.toString().slice(0, 5);
+    document.getElementById("rank2").innerHTML += `<pre>${JSON.stringify(json2.playerInfo.rank, null, 2)}</pre>`;
     document.getElementById("name2").innerHTML += playerNameNoQuotes;
     document.getElementById("Acc2").innerHTML += resultaccavg + "%";
+
+    rank1 = JSON.stringify(json1.playerInfo.rank, null, 2);
+    rank2 = JSON.stringify(json2.playerInfo.rank, null, 2);
+    rankdiff = Number(rank1) - Number(rank2);
+    document.getElementById("rankdiff").innerHTML += rankdiff;
 }
